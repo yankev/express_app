3500e07cf49dac1bf8a2a383985bf83bcf7315ff
yankev <kevinn.yan@gmail.com>
2016-07-16 13:38:01 -0400

    changed credentials for db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var pgp = require("pg-promise")();
-var db = pgp("postgres://postgres@localhost:5432/puppies");
+var db = pgp("postgres://kevin:kevin@localhost:5432/puppies");
 
 db.one("SELECT * from pups;")
     .then(function (data) {
-        console.log("DATA:", data.value);
+        console.log("DATA:", data);
     })
     .catch(function (error) {
         console.log("ERROR:", error);
