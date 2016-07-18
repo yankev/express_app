61e8642e2641214487bf8c81de422a5fc47e071e
yankev <kevinn.yan@gmail.com>
2016-07-18 00:21:42 -0400

    add `add_pup` function

--- db.js
+++ db.js
@@ -13,6 +13,19 @@ function select_all(req, res, next) {
 	    });
 }
 
+function add_pup(req, res, next) {
+	p = req.params;
+	list = [p.name, p.breed, p.age, p.sex];
+	db.none("INSERT INTO pups((name, breed, age, sex) VALUES ($1, $2)", list)
+		.then(function () {
+	        console.log('successful insert');
+	    })
+	    .catch(function(err) {
+	    	console.log('error: ', err)
+	    });
+}
+
 module.exports = {
-	select_all: select_all
+	select_all: select_all,
+	add_pup: add_pup
 }
