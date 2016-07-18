bff04bceb1f6b0f5c09ac0bc4e7196a15dd36d12
yankev <kevinn.yan@gmail.com>
2016-07-17 21:54:54 -0400

    updated the basic server

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 var pgp = require("pg-promise")();
 var db = pgp("postgres://kevin:kevin@localhost:5432/puppies");
 
-db.one("SELECT * from pups;")
-    .then(function (data) {
-        console.log("DATA:", data);
-    })
-    .catch(function (error) {
-        console.log("ERROR:", error);
-    });
+function select_all(req, res, next) {
+
+	db.any("SELECT * from pups;")
+	    .then(function (data) {
+	        console.log("DATA:", data);
+	        res.send(data);
+	    })
+	    .catch(function (error) {
+	        console.log("ERROR:", error);
+	    });
+}
+
+module.exports = {
+	select_all: select_all
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var app = express()
+var app = express();
+var router = require('./routes');
 
-app.get('/', function (req, res) {
-	res.send('Hello World');
-});
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use('/', router);
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
